e8312b91f283f635a9e216424461eebe5f1cd2f4
HD-Sunday <104161557+HD-Sunday@users.noreply.github.com>
2024-11-02 20:38:38 +0300

    Task № 2
    Стилистическое преобразование чисел:
    ➔ Напишите программу, которая запрашивает у пользователя последовательно день
    его рождения, месяц и год;
    ➔ Напишите функцию, которая определяет какому дню недели соответствует эта
    дата?
    ➔ Напишите функцию, которая определяет - високосный это был год, или нет?
    ➔ Напишите функцию, которая определяет сколько сейчас лет пользователю;
    ➔ Реализуйте вывод в консоль даты рождения пользователя в формате дд мм гггг,
    где цифры прорисованы звёздочками (*), как на электронном табло.

---
+++ main.py
@@ -0,0 +1,42 @@
+# Функция для определения дня недели по дате
+def get_day_of_week(day, month, year):
+    # Используем библиотеку datetime для расчета дня недели
+    from datetime import datetime
+    date = datetime(year, month, day)
+    return date.strftime("%A")
+
+# Функция для определения високосного года
+def is_leap_year(year):
+    return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
+
+# Функция для определения возраста пользователя
+def get_age(current_year, birth_year):
+    return current_year - birth_year
+
+# Функция для вывода даты рождения в формате *dd mm yyyy*
+def print_birthday(day, month, year):
+    # Используем форматирование строки для вывода даты
+    print("*" * day + " " * (2 - len(str(month))) + str(month) + " " * (4 - len(str(year))) + str(year) + "*" * 4)
+
+# Запрашиваем у пользователя дату рождения
+day = int(input("Введите день рождения: "))
+month = int(input("Введите месяц рождения (число от 1 до 12): "))
+year = int(input("Введите год рождения: "))
+
+# Выводим дату рождения в формате *dd mm yyyy*
+print_birthday(day, month, year)
+
+# Определяем день недели по дате рождения
+day_of_week = get_day_of_week(day, month, year)
+print("Ваш день рождения приходится на", day_of_week)
+
+# Определяем, был ли год високосным
+leap_year = is_leap_year(year)
+if leap_year:
+    print("Год", year, "был високосным.")
+else:
+    print("Год", year, "не был високосным.")
+
+# Определяем возраст пользователя
+age = get_age(current_year=2024, birth_year=year)
+print("Вам сейчас", age, "лет.")
